707524d5341c2ae607ff1749f10a5b18df2349e7
PriceWu <pricewu@google.com>
2023-04-27 05:29:34 +0000

    update: 1.  update register

--- equipments/series_basis/modem_usb_serial/serial_series.py
+++ equipments/series_basis/modem_usb_serial/serial_series.py
@@ -776,25 +776,25 @@ class AtCmd:
         if tech in ['LTE', 'FR1']:
             if tx_path == 'TX1':
                 if band in [26, 5, 8, 12, 13, 14, 17, 18, 19, 20, 28, 29, 71, 24]:
-                    mipi_num, usid, addr = 0, 4, 0
+                    mipi_num, usid, addr = 0, 4, 1
                 elif band in [1, 2, 3, 4, 66, 7, 25, 30, 38, 41, 40, 39, 34, 70, 75, 76, ]:
-                    mipi_num, usid, addr = 2, 5, 0
+                    mipi_num, usid, addr = 2, 5, 1
                 elif band in [42, 48, 77, 78, 79, ]:
-                    mipi_num, usid, addr = 0, 4, 0
+                    mipi_num, usid, addr = 0, 4, 1
 
             elif tx_path == 'TX2':
                 if band in [1, 2, 3, 4, 66, 7, 25, 30, 38, 41, 40, 39, 34, 70, 75, 76, ]:
                     mipi_num, usid, addr = 0, 4, 0
                 elif band in [42, 48, 77, 78, 79, ]:
-                    mipi_num, usid, addr = 2, 5, 0
+                    mipi_num, usid, addr = 2, 5, 1
                 elif band in [26, 5, 8, 12, 13, 14, 17, 18, 19, 20, 28, 29, 71, 24]:
-                    mipi_num, usid, addr = 0, 4, 0
+                    mipi_num, usid, addr = 0, 4, 1
 
         elif tech in ['WCDMA']:
             if band in [1, 2, 4]:
-                mipi_num, usid, addr = 2, 5, 0
+                mipi_num, usid, addr = 2, 5, 1
             elif band in [5, 8, 6, 19]:
-                mipi_num, usid, addr = 0, 4, 0
+                mipi_num, usid, addr = 0, 4, 1
 
         return mipi_num, usid, addr
 
