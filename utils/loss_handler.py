e84aa18493f648424e0aae137526285f715e617a
Allen Wu <d1658999@gmail.com>
2023-07-09 11:42:01 +0800

    first split different losses to different ports first, which needs continue to modify the function to scripts

--- utils/loss_handler.py
+++ utils/loss_handler.py
@@ -1,11 +1,22 @@
 import csv
 from pathlib import Path
 
+loss_port = {
+    1: 'loss_1.csv',
+    2: 'loss_2.csv',
+    3: 'loss_3.csv',
+    4: 'loss_4.csv',
+    5: 'loss_5.csv',
+    6: 'loss_6.csv',
+    7: 'loss_7.csv',
+    8: 'loss_8.csv',
+}
 
-def read_loss_file():
+
+def read_loss_file(port):
     # file_path = Path('loss.csv')  # test use
     # file_path = Path.cwd().parents[1] / Path('utils') / Path('loss.csv')  # test use
-    file_path = Path('utils') / Path('loss.csv')
+    file_path = Path('utils') / Path(loss_port[port])
     with open(file_path, 'r') as csvfile:
         rows = csv.reader(csvfile)
         next(rows)  # skip the title
@@ -15,8 +26,13 @@ def read_loss_file():
         return loss_dict
 
 
-def get_loss(freq):
-    loss_table_dict = read_loss_file()
+def get_loss(freq, port, port_table_en=False):
+    if port_table_en:
+        pass
+    else:
+        port = 1
+
+    loss_table_dict = read_loss_file(port)
     want_loss = None
     for f in loss_table_dict:
         if freq >= int(f) * 1000:
