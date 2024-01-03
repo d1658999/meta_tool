d36c8615d4d22671c580a0c53f60d65075a6efce
PriceWu <@google.com>
2024-01-03 10:31:25 +0800

    update: New to add exception folder and custom_exception.py for judge what happened in the log

---
+++ exception/custom_exception.py
@@ -0,0 +1,17 @@
+class DictionaryException(Exception):
+    pass
+
+class DataException(Exception):
+    pass
+
+class ComportException(Exception):
+    pass
+
+class GbipException(Exception):
+    pass
+
+class PortTableException(Exception):
+    pass
+
+class FilePlotBwException(Exception):
+    pass
